cc9dcc6f24ae14c77754831e668215aa4c8eb79c
noname55804 <marlin2201@gmail.com>
2015-04-03 17:35:28 +0200

    shamu: get ready for 5.1
    Change-Id: I2fabcf26ccdc9f9cd0956c88efbe2821f57685f2
    Signed-off-by: noname55804 <marlin2201@gmail.com>

---
+++ ch.mk
@@ -0,0 +1,52 @@
+################OPTIMIZATIONS###################
+#Use Optimizations?
+USE_OPTIMIZATIONS := true
+
+ifneq ($(strip $(USE_OPTIMIZATIONS)),false)
+#HACKIFY CONFIGURATION 
+
+##Define ROM toolchain and LIB Verison(should be the same version number as the toolchain)
+TARGET_GCC_AND := 4.8-sm
+TARGET_LIB_VERSION := 4.8
+
+##Define Kernel toolchain
+TARGET_GCC_ARM := 4.9-sm
+
+##Enable Pthread (only on newer devices)
+ENABLE_PTHREAD := true
+
+##Enable O3 Optimizations
+O3_OPTIMIZATIONS:= false
+
+##Enable ArchiDroid Optimizations
+ARCHIDROID_OPTIMIZATIONS := true
+endif
+#################################################
+
+##If you´re running an old device, set this to true
+USE_LEGACY_GCC := false
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1440
+TARGET_SCREEN_WIDTH := 2560
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/ch/config/common_full_phone.mk)
+
+# Enhanced NFC
+$(call inherit-product, vendor/ch/config/nfc_enhanced.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/moto/shamu/aosp_shamu.mk)
+
+DEVICE_PACKAGE_OVERLAYS += device/moto/shamu/overlay-cm
+
+## Device identifier. This must come after all inclusions
+PRODUCT_NAME := ch_shamu
+PRODUCT_BRAND := google
+PRODUCT_MODEL := Nexus 6
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=shamu \
+    BUILD_FINGERPRINT=google/shamu/shamu:5.1/LMY47I/1767468:user/release-keys \
+    PRIVATE_BUILD_DESC="shamu-user 5.1 LMY47I 1767468 release-keys"
